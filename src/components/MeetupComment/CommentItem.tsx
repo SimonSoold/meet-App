b93081b0b1ba867f93d8410307091caf5960e284
John Doe <soold.simon@gmail.com>
2021-12-16 22:57:34 +0100

    whos comment?

--- src/components/MeetupComment/CommentItem.tsx
+++ src/components/MeetupComment/CommentItem.tsx
@@ -1,12 +1,25 @@
 import Comment from "../../models/Comment"
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store'
 
 type Props = {
     comment: Comment 
 }
 
 const CommentItem = ({comment}:Props) => {
+    const user = useSelector((state: RootState) => state.users.user)
+    const meetup = useSelector((state: RootState) => state.meetups.list[state.meetups.index])
+    const checkAuthor = () => {
+        if (user && comment.userId === user.id) {
+            return " yourComment"
+        } else if (comment.userId === meetup.organiserId) {
+            return " authorComment"
+        } else {
+            return " userComment"
+        }
+    }
     return (
-        <li className="commentItem">
+        <li className={"commentItem" + checkAuthor()}>
             <h5>{comment.name}</h5>
             <p>{comment.description}</p>
         </li>
