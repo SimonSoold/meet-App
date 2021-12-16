724c37f56f32e46baa1155eec13c80e83231a7a3
John Doe <soold.simon@gmail.com>
2021-12-16 23:05:32 +0100

    fix provider issues

--- src/components/MeetupComment/CommentItem.test.tsx
+++ src/components/MeetupComment/CommentItem.test.tsx
@@ -4,9 +4,12 @@ import CommentItem from "./CommentItem"
 
 import {meetupList} from "../../testData"
 
+import { Provider } from 'react-redux'
+import { store } from '../../store'
+
 describe("CommentItem", () => {
     it("renders h5 in CommentItem without errors (smoke test)", () => {
-      render(<CommentItem comment={meetupList[0].comments[0]} />)
+      render(<Provider store={store}> <CommentItem comment={meetupList[0].comments[0]} /> </Provider>)
       const h5 = screen.getByText(/Vad fint/i);
       expect(h5).toBeInTheDocument();
     })
--- src/components/MeetupComment/CommentList.test.tsx
+++ src/components/MeetupComment/CommentList.test.tsx
@@ -4,9 +4,12 @@ import CommentList from "./CommentList"
 
 import {meetupList} from "../../testData"
 
+import { Provider } from 'react-redux'
+import { store } from '../../store'
+
 describe("CommentList", () => {
     it("renders h5 in CommentItem without errors (smoke test)", () => {
-      render(<CommentList commentList={meetupList[0].comments} />)
+      render(<Provider store={store}> <CommentList commentList={meetupList[0].comments} /> </Provider>)
       const h5 = screen.getByText(/Vad fint/i);
       expect(h5).toBeInTheDocument();
     })
